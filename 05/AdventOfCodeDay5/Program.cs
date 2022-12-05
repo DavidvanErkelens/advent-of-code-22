46f02145de75943b5e520a059ec454c9d02aa6f7
David van Erkelens <david.vanerkelens@coolblue.nl>
2022-12-05 09:37:29 +0100

    Day #5

---
+++ 05/AdventOfCodeDay5/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Text.RegularExpressions;
+
+string text = File.ReadAllText("../../../input/input.txt");
+string[] parts = text.Split("\n\n");
+
+string[] crateLines = parts[0].Split("\n").Reverse().ToArray();
+char[][] explodedCrateLines = crateLines.Select(x => x.ToCharArray()).ToArray();
+
+char[] topLine = explodedCrateLines[0];
+
+Dictionary<int, Stack<char>> stacks = new Dictionary<int, Stack<char>>();
+
+for (int i = 0; i < topLine.Length; i++)
+{
+    if (topLine[i] != ' ')
+    {
+        int number = topLine[i] - '0';
+        var stack = new Stack<char>();
+
+        for (int j = 1; j < crateLines.Length; j++)
+        {
+            char element = explodedCrateLines[j][i];
+            if (element != ' ') stack.Push(element);
+            else break;
+        }
+
+        stacks.Add(number, stack);
+    }
+}
+
+PrintCrates(stacks);
+
+string[] instructions = parts[1].Split("\n");
+Regex instructionRegex = new Regex("move ([0-9]+) from ([0-9]+) to ([0-9]+)");
+
+foreach (var instruction in instructions)
+{
+    var match = instructionRegex.Match(instruction);
+    var number = Int32.Parse(match.Groups[1].Captures[0].Value);
+    var from = Int32.Parse(match.Groups[2].Captures[0].Value);
+    var to = Int32.Parse(match.Groups[3].Captures[0].Value);
+
+    // Part 1:
+    // for (int i = 0; i < number; i++)
+    // {
+    //     char item = stacks[from].Pop();
+    //     stacks[to].Push(item);
+    // }
+
+    // Part 2:
+    char[] items = new char[number];
+    for (int i = 0; i < number; i++)
+    {
+        items[i] = stacks[from].Pop();
+    }
+
+    foreach (var item in items.Reverse())
+    {
+        stacks[to].Push(item);
+    }
+}
+
+PrintCrates(stacks);
+GetOutput(stacks);
+
+void PrintCrates(Dictionary<int, Stack<char>> stacks)
+{
+    foreach (KeyValuePair<int,Stack<char>> stack in stacks)
+    {
+        Console.Write($"{stack.Key}: ");
+        foreach (var c in stack.Value.Reverse())
+        {
+            Console.Write($"[{c}] ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void GetOutput(Dictionary<int, Stack<char>> stacks)
+{
+    Console.Write("The value: ");
+    foreach (var stack in stacks.Values)
+    {
+        Console.Write(stack.Peek());
+    }
+
+    Console.WriteLine();
+}
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Repository containing implementation for the challenges of 2022's [Advent of Cod
 2. [PHP](02/php) (⭐⭐) / [Rust](02/rust) (⭐⭐)
 3. [PHP](03/php) (⭐⭐) / [Swift + SwiftUI macOS app](03/swift) (⭐⭐)
 4. [PHP](04/php) (⭐⭐) / [C++](04/cpp) (⭐⭐)
+5. [C#](05) (⭐⭐)
